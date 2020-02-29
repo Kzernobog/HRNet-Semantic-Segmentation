7d27ce84679daed0b820910f0b1c2f7bf1824d1a
Kzernobog <adityakamireddypalli@gmail.com>
2020-03-01 02:13:26 +0530

    [Trivial]

--- lib/core/function.py
+++ lib/core/function.py
@@ -268,7 +268,7 @@ def testval(config, test_dataset, testloader, model, evaluator, writer_dict,
 
     with torch.no_grad():
         for index, batch in enumerate(t_val_bar):
-            if count > 100:
+            if count > 1:
                 model.eval()
             count += 1
             image, context, label, _, name = batch
